7fad8d1b597cf9c55ef01cd63bbf2818b719235b
Rafael Barbosa <rb.trobot95@gmail.com>
2023-09-20 15:37:08 -0300

    First commit

---
+++ 05_FirstStepInJavaWithSpringBoot/rest-spring-java-erudio/src/test/java/com/spw/rest/StartupTests.java
@@ -0,0 +1,13 @@
+package com.spw.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
